e6cf9717b4a05eda180f35856d4c10cf00b22e8d
Sadegh <sad3q.msm@gmail.com>
2025-08-02 17:18:28 +0330

    توضیح کوتاه درباره این آپدیت

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function sendToBot() {
+function sendData() {
+  const name = document.getElementById("name").value;
+  const phone = document.getElementById("phone").value;
+
+  if (!name || !phone) {
+    alert("لطفاً همه فیلدها را پر کنید");
+    return;
+  }
+
+  const data = JSON.stringify({ name, phone });
+
   const tg = window.Telegram.WebApp;
-  const name = document.getElementById('nameInput').value;
-  tg.sendData(name);  // ارسال به ربات
+  tg.sendData(data);  // ارسال به ربات
 }
